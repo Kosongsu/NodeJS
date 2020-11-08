16ad339b3cf6cabc98e87820dd478fb61747b479
ghzm0264 <ghzm0264@gmail.com>
2020-11-08 20:56:44 +0900

    mongodb

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://songsu:qorak1@boilerplate.1h9ef.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+
+
+
 app.get('/', (req, res) => res.send('Hello world 하위하위'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
