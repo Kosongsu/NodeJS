45b2dd0fffeeacd540c94938d198299b4724a872
ghzm0264 <ghzm0264@gmail.com>
2020-12-25 01:27:43 +0900

    test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 // express 모듈 가져옴
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
@@ -102,5 +101,5 @@ app.get('/api/users/logout', auth, (req, res) => {
       })
     })
 })
-
+const port = 5000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
